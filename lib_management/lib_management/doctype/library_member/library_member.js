1bc8beea093cf1dd9c87092be877ed21e96b4892
Gatsby2997 <vikashread24@navgurukul.org>
2024-10-17 16:10:43 +0530

    This is Library Management App

---
+++ lib_management/lib_management/doctype/library_member/library_member.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2024, Vikash Kumar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Library Member", {
+	refresh(frm) {
+        frm.add_custom_button('Create Membership', ()=>{
+            frappe.new_doc('Library Membership',{
+                library_member: frm.doc.name
+            })
+        })
+        frm.add_custom_button('Create Transaction', () => {
+            frappe.new.doc('Library Transaction', {
+                library_member: frm.doc.name
+            })
+        })
+
+	},
+});
